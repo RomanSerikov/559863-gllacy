90f49f1e569fa8387d86c677e3577a3de2af176d
Roman Serikov <roman.serikov@yandex.ru>
2017-12-05 22:31:17 +0300

    Add: dark overlay for modal feedback window

--- js/modal-feedback.js
+++ js/modal-feedback.js
@@ -2,6 +2,7 @@ var link = document.querySelector('.button.feedback-link');
   
 var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
+var overlay = document.querySelector(".modal-overlay");
 
 var form    = popup.querySelector("form");
 var author  = popup.querySelector("[name=feedback-author]");
@@ -13,6 +14,7 @@ var storage = localStorage.getItem("author");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  overlay.classList.add("overlay-show");
   
   if (storage) {
     author.value = storage;
@@ -26,6 +28,7 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
+  overlay.classList.remove("overlay-show");
 });
 
 form.addEventListener("submit", function (evt) {
@@ -44,6 +47,7 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
       popup.classList.remove("modal-error");
+      overlay.classList.remove("overlay-show");
     }
   }
 });
