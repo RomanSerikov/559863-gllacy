8153a02d1a5be0f4dccdee83ed3999cb7008b0ad
Roman Serikov <roman.serikov@yandex.ru>
2017-12-06 23:26:54 +0300

    Fix: some bugs and add minification

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+ymaps.ready(init);
+var mapDiv = document.querySelector('.yandex-map');
+var myMap, 
+    myPlacemark;
+
+function init() { 
+  myMap = new ymaps.Map(mapDiv, {
+    center: [59.938880, 30.328430],
+    zoom: 16
+  }); 
+  
+  myPlacemark = new ymaps.Placemark([59.938691, 30.323044], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/pin.png',
+    iconImageSize: [218, 142],
+    iconImageOffset: [-45, -132]
+  });
+  
+  myMap.geoObjects.add(myPlacemark);
+}
+
 var link = document.querySelector('.button.feedback-link');
   
 var popup = document.querySelector(".modal-feedback");
